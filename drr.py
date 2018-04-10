3bf9a04f20d8ad4bfbc0e440748914e26c4cbef1
EricShay <erichsueh1337@hotmail.com>
2018-04-10 16:00:39 -0600

    added movement to drr

--- drr.py
+++ drr.py
@@ -6,6 +6,27 @@ import smach
 import smach_ros
 from time import sleep
 
+def move(x,y):
+    pose = [(x,y,0.0),(0.0,0.0,0.0,1.0)]
+    print(pose)
+    goal = goal_pose(pose)
+    client.send_goal(goal)
+    client.wait_for_result()
+    return
+
+def goal_pose(pose):
+    goal_pose = MoveBaseGoal()
+    goal_pose.target_pose.header.frame_id = 'map'
+    goal_pose.target_pose.pose.position.x = pose[0][0]
+    goal_pose.target_pose.pose.position.y = pose[0][1]
+    goal_pose.target_pose.pose.position.z = pose[0][2]
+    goal_pose.target_pose.pose.orientation.x = pose[1][0]
+    goal_pose.target_pose.pose.orientation.y = pose[1][1]
+    goal_pose.target_pose.pose.orientation.z = pose[1][2]
+    goal_pose.target_pose.pose.orientation.w = pose[1][3]
+    return goal_pose
+
+
 #define State WaitForRequest
 class WaitForRequest(smach.State):
     def __init__(self):
