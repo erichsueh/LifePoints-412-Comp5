6cd729730fec7aabc3f905b60a7fbbb52f6839da
msquigle <msquigle@ualberta.ca>
2018-04-11 11:53:05 -0600

    most transitions done

--- drr.py
+++ drr.py
@@ -4,7 +4,9 @@ import rospy
 from std_msgs.msg import String
 import smach
 import smach_ros
+import cv2
 from time import sleep
+import os
 
 def move(x,y):
     pose = [(x,y,0.0),(0.0,0.0,0.0,1.0)]
@@ -48,11 +50,12 @@ class WaitForRequest(smach.State):
 
 
     def execute(self, userdata):
+        global docked
         rospy.loginfo('Executing state WAIT_FOR_REQUEST')
         self.task = "-1"
         self.counter = 0
         
-        while(self.task == "-1" and self.counter <= 10):
+        while((self.task == "-1" and self.counter <= 10) or (self.task == "-1" and docked)):
             self.sendRequest()
             sleep(1)
             self.counter += 1
@@ -61,31 +64,26 @@ class WaitForRequest(smach.State):
         if(self.task == "-1"):
             return 'returnToBase'
         else:
+            docked = False
             print self.task
-            i = msg.find("x")
-            j = msg.find("y")
-            xPos = int(msg[i+1:j])
-            yPos = int(msg[j+1:])
+            i = self.task.find("x")
+            j = self.task.find("y")
+            xPos = int(self.task[i+1:j])
+            yPos = int(self.task[j+1:])
             userdata.taskOut = [float(xPos)*0.05 - 25, float(yPos)*-0.05 + 23]
             return 'moveToRequest'
 
 #define State ReturnToBase
 class ReturnToBase(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['dockAtBase', 'moveToRequest'])
+        smach.State.__init__(self, outcomes=['dockAtBase'])
         self.counter = 0
 
     def execute(self, userdata):
         rospy.loginfo('Executing state RETURN_TO_BASE')
 
-        if self.counter == 0:
-            self.counter += 1
-            sleep(2)
-            return 'dockAtBase'
-        elif self.counter == 1:
-            self.counter = 0
-            sleep(2)
-            return 'moveToRequest'
+        sleep(2)
+        return 'dockAtBase'
 
 #define State DockAtBase
 class DockAtBase(smach.State):
@@ -93,20 +91,26 @@ class DockAtBase(smach.State):
         smach.State.__init__(self, outcomes=['waitForRequest'])
         
     def execute(self, userdata):
+        global docked
         rospy.loginfo('Executing state DOCK_AT_BASE')
 
         sleep(2)
+        docked = True
         return 'waitForRequest'
 
 #define State MoveToRequest
 class MoveToRequest(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=['waitForRequest', 'waitForLoad'],
-                                    input_keys=['taskIn'])
+                                    input_keys=['taskIn'],
+                                    output_keys=['taskStart'])
         self.counter = 0
         self.task = ""
 
     def execute(self, userdata):
+        #Remove when done testing
+        return 'waitForLoad'
+        #//
         rospy.loginfo('Executing state MOVE_TO_REQUEST')
         self.task = userdata.taskIn
         #adding the move here
@@ -121,39 +125,72 @@ class MoveToRequest(smach.State):
         elif self.counter == 1:
             self.counter = 0
             sleep(2)
+            userdata.taskStart = self.task
             return 'waitForLoad'
 
 #define State WaitForLoad
 class WaitForLoad(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['waitForRequest', 'makeDelivery'])
+        smach.State.__init__(self, outcomes=['waitForRequest', 'makeDelivery'],
+                                    input_keys=['taskStart'],
+                                    output_keys=['destOut', 'taskStart'])
         self.counter = 0
+        self.xPos = -1
+        self.yPos = -1
+        self.haveCursor = True
+
+    def getCursor(self, event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.xPos = x
+            self.yPos = y
+            self.haveCursor = False
+            print x
+            print y
         
     def execute(self, userdata):
         rospy.loginfo('Executing state WAIT_FOR_LOAD')
-        
-        if self.counter == 0:
-            self.counter += 1
-            sleep(2)
-            return 'waitForRequest'
-        elif self.counter == 1:
-            self.counter = 0
-            sleep(2)
-            return 'makeDelivery'
+        self.xPos = -1
+        self.yPos = -1
+        self.haveCursor = True
+        os.system("clear")
+        cancel = raw_input('Press enter when loaded (q for cancellation): ')
+
+        if(cancel != 'q'):
+            cv2.namedWindow("Map")
+            cv2.setMouseCallback("Map", self.getCursor)
+            map = cv2.imread("map.png")
+            cv2.imshow("Map", map)
+
+            while(self.haveCursor):   
+                k = cv2.waitKey(5) & 0xFF
+                if k == 27:
+                    break
+
+            cv2.destroyWindow("Map")
+
+            if(self.xPos != -1):
+                userdata.destOut = [float(self.xPos)*0.05 - 25, float(self.yPos)*-0.05 + 23]
+                return 'makeDelivery'
+
+        return 'waitForRequest'
 
 #define State MakeDelivery
 class MakeDelivery(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['returnToSender', 'waitForUnload'])
+        smach.State.__init__(self, outcomes=['returnToSender', 'waitForUnload'],
+                                    input_keys=['destIn', 'taskStart'],
+                                    output_keys=['taskStart'])
         self.counter = 0
+        self.dest = []
 
     def execute(self, userdata):
         rospy.loginfo('Executing state MAKE_DELIVERY')
+        self.dest = userdata.destIn
 
         if self.counter == 0:
             self.counter += 1
             sleep(2)
-            return 'returnToSender'
+            return 'waitForUnload'
         elif self.counter == 1:
             self.counter = 0
             sleep(2)
@@ -162,7 +199,9 @@ class MakeDelivery(smach.State):
 #define ReturnToSender
 class ReturnToSender(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['waitForLoad', 'waitForRequest'])
+        smach.State.__init__(self, outcomes=['waitForLoad', 'waitForRequest'],
+                                    input_keys=['taskStart'],
+                                    output_keys=['taskStart'])
         self.counter = 0
 
     def execute(self, userdata):
@@ -180,22 +219,24 @@ class ReturnToSender(smach.State):
 #define WaitForUnload
 class WaitForUnload(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['returnToSender', 'waitForRequest'])
+        smach.State.__init__(self, outcomes=['returnToSender', 'waitForRequest'],
+                                    input_keys=['taskStart'],
+                                    output_keys=['taskStart'])
         self.counter = 0
 
     def execute(self, userdata):
         rospy.loginfo('Executing state WAIT_FOR_UNLOAD')
+        os.system("clear")
+        response = raw_input('Press enter when unloaded (q to Return to Sender): ')
 
-        if self.counter == 0:
-            self.counter += 1
-            sleep(2)
-            return 'returnToSender'
-        elif self.counter == 1:
-            self.counter = 0
-            sleep(2)
+        if(response != 'q'):
             return 'waitForRequest'
 
+        return 'returnToSender'
+
 def main():
+    global docked
+    docked = True
     rospy.init_node('jeeves_botSM')
 
     #Create a smach state machine
@@ -206,20 +247,24 @@ def main():
                                 transitions={'returnToBase':'RETURN_TO_BASE','moveToRequest':'MOVE_TO_REQUEST'},
                                 remapping={'taskOut':'task'})
         smach.StateMachine.add('RETURN_TO_BASE', ReturnToBase(), 
-                                transitions={'dockAtBase':'DOCK_AT_BASE','moveToRequest':'MOVE_TO_REQUEST'})
+                                transitions={'dockAtBase':'DOCK_AT_BASE'})
         smach.StateMachine.add('DOCK_AT_BASE', DockAtBase(), 
                                 transitions={'waitForRequest':'WAIT_FOR_REQUEST'})
         smach.StateMachine.add('MOVE_TO_REQUEST', MoveToRequest(), 
                                 transitions={'waitForRequest':'WAIT_FOR_REQUEST','waitForLoad':'WAIT_FOR_LOAD'},
-                                remapping={'taskIn':'task'})
+                                remapping={'taskIn':'task','taskStart':'taskStart'})
         smach.StateMachine.add('WAIT_FOR_LOAD', WaitForLoad(), 
-                                transitions={'waitForRequest':'WAIT_FOR_REQUEST','makeDelivery':'MAKE_DELIVERY'})
+                                transitions={'waitForRequest':'WAIT_FOR_REQUEST','makeDelivery':'MAKE_DELIVERY'},
+                                remapping={'destOut':'dest','taskStart':'taskStart'})
         smach.StateMachine.add('MAKE_DELIVERY', MakeDelivery(), 
-                                transitions={'returnToSender':'RETURN_TO_SENDER','waitForUnload':'WAIT_FOR_UNLOAD'})
+                                transitions={'returnToSender':'RETURN_TO_SENDER','waitForUnload':'WAIT_FOR_UNLOAD'},
+                                remapping={'destIn':'dest','taskStart':'taskStart'})
         smach.StateMachine.add('RETURN_TO_SENDER', ReturnToSender(), 
-                                transitions={'waitForLoad':'WAIT_FOR_LOAD','waitForRequest':'WAIT_FOR_REQUEST'})
+                                transitions={'waitForLoad':'WAIT_FOR_LOAD','waitForRequest':'WAIT_FOR_REQUEST'},
+                                remapping={'taskStart':'taskStart'})
         smach.StateMachine.add('WAIT_FOR_UNLOAD', WaitForUnload(), 
-                                transitions={'returnToSender':'RETURN_TO_SENDER','waitForRequest':'WAIT_FOR_REQUEST'})
+                                transitions={'returnToSender':'RETURN_TO_SENDER','waitForRequest':'WAIT_FOR_REQUEST'},
+                                remapping={'taskStart':'taskStart'})
 
     sis = smach_ros.IntrospectionServer('JEEVES_server', sm_stk, 'drr')
     sis.start()
